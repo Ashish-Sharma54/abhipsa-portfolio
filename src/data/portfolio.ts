93b55178d19a474d56e42e0ca4df733c928e2399
Ashish Sharma <officialashish.332028@gmail.com>
2026-07-20 22:15:22 +0530

    Fix CV download with programmatic link click

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -8,7 +8,7 @@ export const siteConfig = {
   github: "https://github.com/dashabhipsa",
   linkedin: "https://www.linkedin.com/in/dash-abhipsa1a00121/",
   location: "NIT Rourkela, Odisha",
-  resumeUrl: "https://drive.google.com/uc?export=download&id=19moOUDXGppFoUSiFKzdl9uZPFpJMyfSN",
+  resumeUrl: "https://drive.google.com/uc?export=download&confirm=t&id=19moOUDXGppFoUSiFKzdl9uZPFpJMyfSN",
 };
 
 export const navLinks = [
